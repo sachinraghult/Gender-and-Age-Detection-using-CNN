24b6f1f3bde4e793dc4948e536ff5677bd05a0e7
sachinraghult <sachinraghult2002@gmail.com>
2022-01-07 21:54:30 +0530

    face detection and pixel reduction

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #import libraries
 import numpy as np
+import pandas as pd
 import string
 import re
 import pickle
@@ -7,6 +8,7 @@ import cv2
 import os
 import csv
 import shutil
+import tensorflow as tf
 import matplotlib.pyplot as plt
 from flask import Flask, render_template, request
 
@@ -17,40 +19,6 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
-
-def detect_face():
-    classifier = cv2.CascadeClassifier(cv2.data.haarcascades+"haarcascade_frontalface_default.xml")
-    dirFace = 'static/cropped_face'
-
-    # Create if there is no cropped face directory
-    if not os.path.exists(dirFace):
-        os.mkdir(dirFace)
-        print("Directory " , dirFace ,  " Created ")
-    else:    
-        print("Directory " , dirFace ,  " has found.")
-
-
-    path = r'static/file.jpg'
-
-    im = cv2.imread(path, 0)
-
-    # detectfaces 
-    faces = classifier.detectMultiScale(
-        im, # stream 
-        scaleFactor=1.10, # change these parameters to improve your video processing performance
-        minNeighbors=20, 
-        minSize=(48, 48) # min image detection size
-        ) 
-
-    # Draw rectangles around each face
-    for (x, y, w, h) in faces:
-
-        cv2.rectangle(im, (x, y), (x + w, y + h),(0,0,255),thickness=2)
-        # saving faces according to detected coordinates 
-        sub_face = im[y:y+h, x:x+w]
-        FaceFileName = "static/cropped_face/face_" + str(y+x) + ".jpg" # folder path and random name image
-        cv2.imwrite(FaceFileName, sub_face)
-
 
 def reduce_pixels():
     # assign directory
@@ -66,8 +34,6 @@ def reduce_pixels():
         if os.path.isfile(f):
             image = cv2.imread(f, 0)
             img = cv2.resize(image, (48, 48))
-            plt.imshow(img)
-            plt.show()
 
             #extracting pixels from image
             rows,cols = img.shape
@@ -101,16 +67,52 @@ def reduce_pixels():
     #remove the folder after testing
     
 
+def detect_face():
+    classifier = cv2.CascadeClassifier(cv2.data.haarcascades+"haarcascade_frontalface_default.xml")
+    dirFace = 'static/cropped_face'
+
+    # Create if there is no cropped face directory
+    if not os.path.exists(dirFace):
+        os.mkdir(dirFace)
+        print("Directory " , dirFace ,  " Created ")
+    else:    
+        print("Directory " , dirFace ,  " has found.")
+
+
+    path = r'static/file.jpg'
+
+    im = cv2.imread(path, 0)
+
+    # detectfaces 
+    faces = classifier.detectMultiScale(
+        im, # stream 
+        scaleFactor=1.10, # change these parameters to improve your video processing performance
+        minNeighbors=20, 
+        minSize=(48, 48) # min image detection size
+        ) 
+
+    # Draw rectangles around each face
+    for (x, y, w, h) in faces:
+
+        cv2.rectangle(im, (x, y), (x + w, y + h),(0,0,255),thickness=2)
+        # saving faces according to detected coordinates 
+        sub_face = im[y:y+h, x:x+w]
+        FaceFileName = "static/cropped_face/face_" + str(y+x) + ".jpg" # folder path and random name image
+        cv2.imwrite(FaceFileName, sub_face)
+    
+
+
+
+
 
 @app.route('/result',methods=['POST'])
 def result():
     if request.method == 'POST':
         img = request.files['uploadImage'];
         img.save("static/file.jpg");
-
         detect_face();
-        reduce_pixels();
 
+    reduce_pixels();
     return render_template('result.html')
 
 if __name__ == "__main__":
